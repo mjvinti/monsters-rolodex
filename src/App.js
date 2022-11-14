395bfea0241494b713412ebfd011204c0b02d3ae
Mario Vinti <mjvinti@gmail.com>
2022-11-14 15:31:32 -0500

    fetching via component did mount

--- src/App.js
+++ src/App.js
@@ -8,13 +8,19 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        { name: "Linda", id: "12e1231e" },
-        { name: "Frank", id: "12ed1dacas" },
-        { name: "Jackie", id: "1231e" },
-        { name: "Andrei", id: "12e1e213" },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => ({ monsters: users }),
+          () => console.log(this.state)
+        )
+      );
   }
 
   render() {
