139a044671104835935cdd1d1f8a928c53ea6c6e
Mario Vinti <mjvinti@gmail.com>
2022-11-15 13:35:01 -0500

    added card list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
 
+import CardList from "./components/card-list/card-list.component";
+
 import "./App.css";
 
 class App extends Component {
@@ -40,9 +42,7 @@ class App extends Component {
           type="search"
         />
         {filteredMonsters.map((monster) => (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
-          </div>
+          <CardList key={monster.id} />
         ))}
       </div>
     );
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class CardList extends Component {
+  render() {
+    return <h1>hello, I'm the card list component</h1>;
+  }
+}
+
+export default CardList;
