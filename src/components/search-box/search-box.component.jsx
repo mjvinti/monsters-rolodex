55d09ab52dd78b17b936365e42cef7888bb62a2a
Mario Vinti <mjvinti@gmail.com>
2022-11-16 17:36:57 -0500

    migrated all components to functional

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,20 +1,12 @@
-import { Component } from "react";
-
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-  render() {
-    const { className, onChangeHandler, placeholder } = this.props;
-
-    return (
-      <input
-        className={`search-box ${className}`}
-        onChange={onChangeHandler}
-        placeholder={placeholder}
-        type="search"
-      />
-    );
-  }
-}
+const SearchBox = ({ className, onChangeHandler, placeholder }) => (
+  <input
+    className={`search-box ${className}`}
+    onChange={onChangeHandler}
+    placeholder={placeholder}
+    type="search"
+  />
+);
 
 export default SearchBox;
